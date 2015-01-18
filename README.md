c51d6b0344e96eb891d2dc55932678f7acc17ced
Ben Lippincott <benji.lippincott@gmail.com>
2015-01-17 21:02:52 -0500

    No changes made

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-camspy
 Show images from a webcam with Node.js
+
+This is for my own personal use, so don't expect very much documentation. However, I will help whoever wants to hack through it :D
+
---
+++ camspy.js
@@ -0,0 +1,17 @@
+// 2015 (C) Ben Lippincott
+
+var Camelot = require('camelot')
+var BinaryServer = require('binaryjs').BinaryServer
+var fs = require('fs')
+
+var binServer = BinaryServer({port : 9000});
+var camelot = new Camelot({
+  'rotate' : '180',
+  'flip' : 'v'
+})
+
+binServer.on('connection',function(client){
+	camelot.grab({'frequency' : 3},function(image){
+		client.send(image)
+	})
+})
