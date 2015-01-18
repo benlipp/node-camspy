4b55633a53fe5a4c9a631f41136c89cf4b9e6290
Ben Lippincott <benji.lippincott@gmail.com>
2015-01-18 10:48:16 -0500

    You can't see it, but I'm making a very angry face right now

--- alert.bash
+++ alert.bash
@@ -5,4 +5,6 @@ time_length=0.25s
 for i in `seq 1 5`;
 do
 	timeout $time_length speaker-test -f500 -t sine;
+	timeout $time_length speaker-test -f1000 -t sine;
+	timeout $time_length speaker-test -f750 -t sine;
 done
